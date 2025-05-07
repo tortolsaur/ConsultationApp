d34d95225ee603c4245be88581048db34ea2764e
Tortolsaur <clientjohnrabusa@gmail.com>
2025-05-07 14:27:20 +0800

    Consultations & Archive

--- Archive.Designer.cs
+++ Archive.Designer.cs
@@ -1,14 +1,14 @@
 ﻿namespace Consultation
 {
-    partial class Form1
+    partial class Archive
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,20 +23,20 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             SuspendLayout();
             // 
-            // Form1
+            // Archive
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1898, 1024);
-            Name = "Form1";
-            Text = "Form1";
+            Name = "Archive";
+            Text = "Archive";
             ResumeLayout(false);
         }
 
